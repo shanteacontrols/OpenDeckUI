72e12ca075d83557beff7524087626d7e1c5517b
Tomislav <tomo.glavas@gmail.com>
2020-09-14 19:37:10 +0200

    Use hash url because GH pages don't rewrite URLs

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { DeviceRoutes } from "./definitions/device";
 
-const history = createWebHistory();
+const history = createWebHashHistory();
 export const router = createRouter({
   history,
   routes: [...DeviceRoutes],
