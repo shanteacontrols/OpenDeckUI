61bef34f19a81da48a2ebf885d9c7906c84df0eb
paradajz <2544094+paradajz@users.noreply.github.com>
2020-10-19 22:11:25 +0200

    activity log: display note on/note off strings properly according to MIDI spec

--- src/definitions/request-log/request-log-store/log-type-midi.ts
+++ src/definitions/request-log/request-log-store/log-type-midi.ts
@@ -90,7 +90,12 @@ export const addMidi = (params: MidiEventParams): void => {
   const note = ["noteon", "noteoff"].includes(type) ? data[1] : undefined;
   const controllerNumber = controller && controller.number;
   const velocity = data && data.length > 2 ? data[2] : undefined;
-  const label = MidiEventTypeLabel[type];
+  const label =
+    type == "noteoff"
+      ? data[0] >= 144
+        ? MidiEventTypeLabel.noteon
+        : MidiEventTypeLabel.noteoff
+      : MidiEventTypeLabel[type];
 
   const dataDec = data && ensureString(dataArray);
   const dataHex = data && ensureString(convertToHexString(dataArray));
