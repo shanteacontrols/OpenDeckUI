61ca0a4405f62ff9d539435735982bb3a1d6b83f
paradajz <2544094+paradajz@users.noreply.github.com>
2026-05-21 06:24:45 +0000

    bless boards

--- src/definitions/blessing/blessed-serials.ts
+++ src/definitions/blessing/blessed-serials.ts
@@ -38,4 +38,100 @@ export const blessedSerials: IBlessedSerialEntry[] = [
     signature:
       "7EQKPdOc7n8NkQDhTdgSbm4TjdjzxhVWRUo57jR9Fqq4zZXuAZev+CcR3rm1F8Na57NXEH/KD7oCvXJXchGCBA==",
   },
+  {
+    version: 1,
+    serialHash:
+      "9BDFA736798BB38E742BF59B79B0BDD021A81EBC0FF3977BB9917CE69A623C56",
+    features: "config",
+    signature:
+      "xreGR2XgfOwxjw3NUV7QTrC/JVD5n7zQ5ocLHjnImzGfZlvDMbtcgFojiI6YlwxGIiRh/TicNlgukl7TItJxDA==",
+  },
+  {
+    version: 1,
+    serialHash:
+      "C9F5BC052504E8DFD7D83A963E7CCAA8947A6DB33A147ADDBD7BFC13E4411E8F",
+    features: "config",
+    signature:
+      "RemRKcPflz9PnFBYIgkzPuSPu493H5aHiXFJkDMLnBHqxFkuLubkxitovqIdMmGc7S0ZI0vM6POvfzxY7mQZCg==",
+  },
+  {
+    version: 1,
+    serialHash:
+      "39101AABC2ECB239089960AEDF1B23C7E402D117E8BB411FB27614F660F6C18E",
+    features: "config",
+    signature:
+      "4kf4dEswmzLygK9nSYLr1pYU0OZnTcg7us036PKWS+hwpgteTBg43+JgpKAnlaYtEjMfrV/MnagsNysIrnxzDQ==",
+  },
+  {
+    version: 1,
+    serialHash:
+      "9A31C37D8052EB0C96F18AC7243A9FA99861499653F1D9357FEC9A10FD5CAAD8",
+    features: "config",
+    signature:
+      "d/QhU4IeeDPgqluyzZH0pkhuUGpYoKT9Y9+ASWHHOHOHg71ijWDTjZIKEQa1VsZsY+sdCWC7Cm0okmQoAa9PCA==",
+  },
+  {
+    version: 1,
+    serialHash:
+      "64189312B46AD8AEE74AB91A9B67B30134F7A0212EE916AEB45FD69FF824CE10",
+    features: "config",
+    signature:
+      "srPerepYRTmLJDG18T80t/tF9/4tGo8+Uhql4nQo+gUjhqE34HGE3qx0cvXh2k70ApXkwHA+5EABtjxJBhuJCA==",
+  },
+  {
+    version: 1,
+    serialHash:
+      "82EBDD1D3ACF9326603402F241DA4159C30955FF72DA6DF66EE14D4CF2750F1E",
+    features: "config",
+    signature:
+      "a6PXRNbm1AQach7bHBab/mdrn0b13cjGfEpNG+j9sNoTbJ+D1qtJjVG71R0WdXj6ORe5u554hW3Wf3+wUertAg==",
+  },
+  {
+    version: 1,
+    serialHash:
+      "88B8D3EA975A6B2AE2B03EDE1C63C2829B48B3957823829FE1554DF438DDC24E",
+    features: "config",
+    signature:
+      "zOpfgW1Wh0VALP8mhKIUimBvBmOhQVJ9NyoSnjIDH26euo/XZe5i7L62b+jgIcngp5cd7OYnW9IQdF/c3Gx5Bg==",
+  },
+  {
+    version: 1,
+    serialHash:
+      "E2AFA5FC8E2AF2C4C440A73E4EC005D365E10BC988732CF795BCC46AC7302DA6",
+    features: "config",
+    signature:
+      "mySYmeFdiim/M776zPD0NWjA+8L2dbkqZvviKR3QOPXkrOcfOl/1dz7l9pWuv1/O8NSxvwadLJVn9VaidkZkAQ==",
+  },
+  {
+    version: 1,
+    serialHash:
+      "AD3C03FF0A57734EFA357756DD98F719C34EF59E4BBEAE6CBFCF64DCDB84CA0C",
+    features: "config",
+    signature:
+      "UqcfhVXSSyxTGslTPuPyZ/d/vGxzh6f6cdjyaGwzdKBRWSY6ZNXwB/B7cvWn2bAxzUUZ5r8DMh/x6obYcLKFAQ==",
+  },
+  {
+    version: 1,
+    serialHash:
+      "64FDEB865A55D8D2DFB311F50CEE856E0C4E923555B5F543C9ED015AA29E1AA1",
+    features: "config",
+    signature:
+      "UACdTXCM239WLZxAMPsRElDkpfopmtzfgilPwqYz/HYRc04zTS0ymbZbFqvE4BGsXRc/aHj4D8CFgFfS0L0dAw==",
+  },
+  {
+    version: 1,
+    serialHash:
+      "8A31A07F95922B908BAA8D8B9D541A90D0D8F928E394DF8D0C994DAE1E7F64C4",
+    features: "config",
+    signature:
+      "0h57SDHdhLZO6vzkBqzQCWj3j1hMU536lY88ItOw9xMW+R2o8eYL95JY5+a6R9jWGvb2yIVjw8HecP4PHve0Bw==",
+  },
+  {
+    version: 1,
+    serialHash:
+      "99D7265C366A81B4EED5DF47CCF9116535B3F11F8E0C77D246B14F984D2D6E30",
+    features: "config",
+    signature:
+      "CT+E5ABPxZk+IdbgkIZ7WmLNSYdi6PyRTOTMBQd+3jOlUtdv71gtHw5M8iHTWFurjTNqde2w2PbKyoBTbcYjAQ==",
+  },
 ];
